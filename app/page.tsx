413b844952dbfecdd3a0d8ed28095b5a38637045
at0x123 <vp.ca94@gmail.com>
2024-01-30 12:58:42 -0500

    royd

--- app/page.tsx
+++ app/page.tsx
@@ -10,20 +10,19 @@ import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { BotScreen } from "./components/AppComponent";
 import Image from "next/image";
 import houseBackground from "../public/RoyLogo.png";
-import { useRoy, advanceRoy, newRoy, Roy } from "@/app/hooks/useRoy";
+import { newRoy, Roy } from "@/app/hooks/useRoy";
 import { useAccount } from "wagmi";
 import { useSigner } from "./utils/wagmi-utils";
 import { EAS, SchemaEncoder } from "@ethereum-attestation-service/eas-sdk";
 import { useBotStore } from "./store/bot";
 import { useGlobalState } from "./store/store";
 import { ChatMessage } from "./store/session";
-import { useEffect, useState } from "react";
 import { RoyAttributes } from "./hooks/useRoy";
 import toast from "react-hot-toast";
 
 const Home: NextPage = () => {
-  const [isMobile, setIsMobile] = useState(false);
-const account = useAccount()
+    const [isMobile, setIsMobile] = useState(false);
+    const account = useAccount()
     const address: `0x${string}` = account.address ? account.address : "0x000";
     const botStore = useBotStore();
     const session = botStore.currentSession();
@@ -189,134 +188,134 @@ const account = useAccount()
 
 
 
-  // attest sending roy
-  // create story
-  // embed results
-  // upload to db
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768); // 768px is a common breakpoint for mobile
-    };
+    // attest sending roy
+    // create story
+    // embed results
+    // upload to db
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 768); // 768px is a common breakpoint for mobile
+        };
 
-    handleResize();
-    window.addEventListener("resize", handleResize);
+        handleResize();
+        window.addEventListener("resize", handleResize);
 
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
-  return (
-    <>
-      <div
-        className={isMobile ? "mobile-layout-class" : "desktop-layout-class"}
-      >
-        
-            <div className="fixed z-10 flex flex-col text-black items-center flex-grow pt-10">
-                <div
-                    className="relative  items-center w-2/3 h-1/2 backdrop-blur-lg"
+    return (
+        <>
+            <div
+                className={isMobile ? "mobile-layout-class" : "desktop-layout-class"}
+            >
 
-                >
-                    <Image
-                        src={roy.attributes?.image || "/image.png"}
-                        fill
-                        alt="bg" />
+                <div className="fixed z-10 flex flex-col text-black items-center flex-grow pt-10">
+                    <div
+                        className="relative  items-center w-2/3 h-1/2 backdrop-blur-lg"
 
-
-                    <div className="px-5">
-
-                        <div
-                            className="top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-gray-900 opacity-80 backdrop-blur-lg text-white p-24">
-                            <h1 className="text-center mb-8">
-                                <span className="block text-2xl mb-2">Welcome to</span>
-                                <span className="block text-4xl font-bold">
-                                    Roy: A Life Well Lived
-                                </span>
-                            </h1>
+                    >
+                        <Image
+                            src={roy.attributes?.image || "/image.png"}
+                            fill
+                            alt="bg" />
 
 
-                            <span
-                                className="cursor-pointer"
-                                onClick={() => updateIndex("Index")}
-                            >Roy:{royIndex + 1} of {filteredRoys.length}</span>
-                            <span
-                                className="cursor-pointer"
-                                onClick={() => updateIndex("Decade")}
-                            >{" "} Decade :{decade + 1} of {filteredRoys[royIndex]?.roy.length}</span>
+                        <div className="px-5">
 
-                            <div className="p-2 border-2 h-[900px]">
-                                <div className="flex flex-row w-full h-full overflow-auto">
+                            <div
+                                className="top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-gray-900 opacity-80 backdrop-blur-lg text-white p-24">
+                                <h1 className="text-center mb-8">
+                                    <span className="block text-2xl mb-2">Welcome to</span>
+                                    <span className="block text-4xl font-bold">
+                                        Roy: A Life Well Lived
+                                    </span>
+                                </h1>
 
-                                    {roy.attributes && (
-                                        <>
-                                            <ul className="p-2 m-2 w-full">
-                                                <li><strong>Name:</strong> {roy.attributes?.name}</li>
-                                                <li><strong>Current Location: </strong> {roy.attributes?.currentLocation}</li>
-                                                <li><strong>Current Year:</strong>  {roy.attributes?.currentYear}</li>
-                                                <li><strong>Current Decade:</strong>  {roy.attributes?.count}</li>
-                                                <li><strong>Resolution:</strong>  {roy?.currentDecadeResolution}</li>
 
-                                                <RoyExperiences />
-                                                <Image
-                                                    src={roy.attributes.image}
-                                                    height={200}
-                                                    width={200}
-                                                    className="border-2"
-                                                    alt="bg" />
-                                            </ul>
+                                <span
+                                    className="cursor-pointer"
+                                    onClick={() => updateIndex("Index")}
+                                >Roy:{royIndex + 1} of {filteredRoys.length}</span>
+                                <span
+                                    className="cursor-pointer"
+                                    onClick={() => updateIndex("Decade")}
+                                >{" "} Decade :{decade + 1} of {filteredRoys[royIndex]?.roy.length}</span>
 
-                                        </>
-                                    )
-                                    }
+                                <div className="p-2 border-2 h-[900px]">
+                                    <div className="flex flex-row w-full h-full overflow-auto">
 
-                                    <BotScreen />
+                                        {roy.attributes && (
+                                            <>
+                                                <ul className="p-2 m-2 w-full">
+                                                    <li><strong>Name:</strong> {roy.attributes?.name}</li>
+                                                    <li><strong>Current Location: </strong> {roy.attributes?.currentLocation}</li>
+                                                    <li><strong>Current Year:</strong>  {roy.attributes?.currentYear}</li>
+                                                    <li><strong>Current Decade:</strong>  {roy.attributes?.count}</li>
+                                                    <li><strong>Resolution:</strong>  {roy?.currentDecadeResolution}</li>
+
+                                                    <RoyExperiences />
+                                                    <Image
+                                                        src={roy.attributes.image}
+                                                        height={200}
+                                                        width={200}
+                                                        className="border-2"
+                                                        alt="bg" />
+                                                </ul>
+
+                                            </>
+                                        )
+                                        }
+
+                                        <BotScreen />
+                                    </div>
+
                                 </div>
+
+
 
                             </div>
 
 
-
                         </div>
-
-
                     </div>
+
+                    <div className="flex-grow w-full mt-16 px-8 py-12">
+                        {/* Other content */}
+                    </div>
+
+                    {/* Play Button */}
+                    <div className="fixed bottom-10 left-1/2 transform -translate-x-1/2 z-10">
+                        <ul className="text-2xl font-bold bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-full  cursor-pointer"
+
+
+                        >
+                            <label
+                            > Name<input value={name} onChange={e => { setName(e.target.value); e.stopPropagation() }} /></label><br />
+                            <label
+                            > Location<input value={location} onChange={e => { setLocation(e.target.value); e.stopPropagation() }} /></label><br />
+                            <label
+                            > Year<input value={year} onChange={e => { setYear(Number(e.target.value)); e.stopPropagation() }} /></label><br />
+                            <button
+                                onClick={() => attestRoy()}
+                            >A Button</button>
+                        </ul>
+                    </div>
+
                 </div>
+                <img
+                    src="/RoyTitleScreen.png"
+                    className="w-full max-w-md mx-auto"
+                    alt="Roy Title Screen"
+                />
 
-                <div className="flex-grow w-full mt-16 px-8 py-12">
-                    {/* Other content */}
-                </div>
-
-                {/* Play Button */}
-                <div className="fixed bottom-10 left-1/2 transform -translate-x-1/2 z-10">
-                    <ul className="text-2xl font-bold bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-full  cursor-pointer"
-
-
-                    >
-                        <label
-                        > Name<input value={name} onChange={e => { setName(e.target.value); e.stopPropagation() }} /></label><br />
-                        <label
-                        > Location<input value={location} onChange={e => { setLocation(e.target.value); e.stopPropagation() }} /></label><br />
-                        <label
-                        > Year<input value={year} onChange={e => { setYear(Number(e.target.value)); e.stopPropagation() }} /></label><br />
-                        <button
-                            onClick={() => attestRoy()}
-                        >A Button</button>
-                    </ul>
-                </div>
-
+                <UpperLeftComponent />
+                <UpperRightComponent />
+                <BottomLeftComponent />
+                <BottomMiddleComponent />
+                <BottomRightComponent />
             </div>
-        <img
-          src="/RoyTitleScreen.png"
-          className="w-full max-w-md mx-auto"
-          alt="Roy Title Screen"
-        />
-
-        <UpperLeftComponent />
-        <UpperRightComponent />
-        <BottomLeftComponent />
-        <BottomMiddleComponent />
-        <BottomRightComponent />
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default Home;
