5d362c04a4a1163de3046baf16ea0d45d2f23ce1
mattim0use <TastyCHAINLINK@proton.me>
2024-01-29 18:10:37 -0800

    matti's attempt to organize, and style

--- app/components/BottomRightComponent.tsx
+++ app/components/BottomRightComponent.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BottomRightComponent: React.FC = () => {
   return (
-    <div className="bg-red-500 border-white border z-10 min-w-[400px] min-h-[400px] mx-10 my-10 fixed bottom-0 right-0 p-4 text-center">
+    <div className="bg-red-500 border-white border rounded-xl z-10 min-w-[400px] min-h-[400px] mx-10 my-10 fixed bottom-0 right-0 p-4 text-center">
       <p>Bottom Right Component</p>
     </div>
   );
